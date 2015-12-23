b58e8d97638cfc453f3084684160bf9c54487086
Brance <292692700@qq.com>
2015-12-23 13:09:16 +0800

    aaa

--- 支付宝demos/frame/IntegratedAlipay/AlipayFiles/AlipayRequestConfig.m
+++ 支付宝demos/frame/IntegratedAlipay/AlipayFiles/AlipayRequestConfig.m
@@ -3,7 +3,7 @@
 //  IntegratedAlipay
 //
 //  Created by Winann on 15/1/9.
-//  Copyright (c) 2015年 Winann. All rights reserved.
+//  Copyright (c) 2015年 Winann. All rights reserved. 
 //
 
 #import "AlipayRequestConfig.h"
